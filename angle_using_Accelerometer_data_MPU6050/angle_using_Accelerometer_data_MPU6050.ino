8b908c71e54104824b8f01144daa0391df03b028
Satyam-36 <216320084@gkv.ac.in>
2024-02-10 17:03:05 +0530

    ...

--- angle_using_Accelerometer_data_MPU6050/angle_using_Accelerometer_data_MPU6050.ino
+++ angle_using_Accelerometer_data_MPU6050/angle_using_Accelerometer_data_MPU6050.ino
@@ -1,22 +1,28 @@
 #include <Wire.h>
 #include<math.h>
-const int MPU = 0x68; // MPU6050 I2C address
+const int mpu = 0x68; // MPU6050 I2C address
 float AccX, AccY, AccZ;
 float angleX,angleY;
 int c=200;
 float angleErrorX=0,angleErrorY=0;
-void setup() {
+
+void setup()
+{
   Serial.begin(9600);
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
+  //Starting mpu6050 in power mode
+  Wire.beginTransmission(mpu);
+  Wire.write(0x6B);
+  Wire.write(0x00);  //All bits in this register has to be set to zero for the device to start and continue in power mode
+  Wire.endTransmission();
   delay(20);
   readAccelError();
+  
 }
+
 void loop() {
   readAccel();
+  angleX-=angleErrorX;
+  angleY-=angleErrorY;
   Serial.print(" x ");
   Serial.print(angleX);
   Serial.print(" | ");
@@ -27,38 +33,32 @@ void loop() {
 void readAccel()
 {
   // === Read acceleromter data === //
-
-    Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
- 
   // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(mpu);
   Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
   Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
   Wire.endTransmission(true);
   
- //Accessing Register to activate low pass filter
-        Wire.beginTransmission(MPU);
-        Wire.write(0x1A);
-        Wire.write(0x05); 
-        Wire.endTransmission();
-        
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  //Activitating Low pass filter
+  Wire.beginTransmission(mpu);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+  //Accessing registers to store accelerometer data     
+  Wire.beginTransmission(mpu);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  //Requesting 6 bytes from mpu6050 to pull accelerometer measurement information
+  Wire.requestFrom(mpu, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
   //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
   AccX = (Wire.read() << 8 | Wire.read()) / 4096.0; // X-axis value
   AccY = (Wire.read() << 8 | Wire.read()) / 4096.0; // Y-axis value
   AccZ = (Wire.read() << 8 | Wire.read()) / 4096.0; // Z-axis value
-
+  // making acceleration in x-axis or y-axis or z-axis equal to 0 expect on which axis acceleration exprienced is equal to 1
   AccX=AccX/0.97;
   AccY=AccY/0.97;
   AccZ=AccZ/0.97;
-
+  //Limitting acceleration value from -1 to 1 in all axies
   if(AccX>1)
   {
     AccX=1;
@@ -67,9 +67,7 @@ void readAccel()
   {
     AccX=-1;
   }
-
   //////////////////////////////
-
    if(AccY>1)
   {
     AccY=1;
@@ -87,83 +85,23 @@ void readAccel()
   {
     AccZ=-1;
   }
+
+ //Calculating angles using Accelerometer data in all 3-axies
  angleX=(atan(AccY/sqrt(pow((AccX),2)+pow((AccZ),2))))*57.2958;
  angleY=(atan(AccX/sqrt(pow((AccY),2)+pow((AccZ),2))))*57.2958;
-
- angleX=angleX-angleErrorX;
- angleY=angleY-angleErrorY;
 }
 
 void readAccelError()
 {
+  Serial.println("Dont't move MPU6050 callibration accelerometer data");
   for(int i=0;i<=c;i++)
   {
-      // === Read acceleromter data === //
-         Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
- 
-  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
-  Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
-  Wire.endTransmission(true);
-  
- //Accessing Register to activate low pass filter
-        Wire.beginTransmission(MPU);
-        Wire.write(0x1A);
-        Wire.write(0x05); 
-        Wire.endTransmission();
-        
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 4096.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 4096.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 4096.0; // Z-axis value
-
-  AccX=AccX/0.97;
-  AccY=AccY/0.97;
-  AccZ=AccZ/0.97;
-
-  if(AccX>1)
-  {
-    AccX=1;
-  }
-   if(AccX<-1)
-  {
-    AccX=-1;
-  }
-
-  //////////////////////////////
-
-   if(AccY>1)
-  {
-    AccY=1;
-  }
-   if(AccY<-1)
-  {
-    AccY=-1;
-  }
-  ////////////////////////////////
-   if(AccZ>1)
-  {
-    AccZ=1;
-  }
-   if(AccZ<-1)
-  {
-    AccZ=-1;
-  }
- angleX=(atan(AccY/sqrt(pow((AccX),2)+pow((AccZ),2))))*57.2958;
- angleY=(atan(AccX/sqrt(pow((AccY),2)+pow((AccZ),2))))*57.2958;
- 
-    angleErrorX=angleErrorX+angleX;
-    angleErrorY=angleErrorY+angleY;
+      readAccel();
+      angleErrorX+=angleX;
+      angleErrorY+=angleY;
+      delay(1);
   }
   angleErrorX=angleErrorX/c;
-  angleErrorY=angleErrorY/c; 
+  angleErrorY=angleErrorY/c;
+  Serial.println("Done callibrationg accelerometer data");
 }
